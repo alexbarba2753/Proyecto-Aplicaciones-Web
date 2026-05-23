ebfd4cec9d6152cd6c0d1a65059a2dec61bee7ae
DannyPoli06 <daniel.diaz01@epn.edu.ec>
2026-05-23 13:36:24 -0500

    Coreccion Vite-Tailwind

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-import './index.css' // <-- Asegúrate de que index.css tenga el punto y la barra
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
