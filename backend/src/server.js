351ed8d7ba818aa52044a3a4203b488d9d1c8bb7
alexbarba2753 <alex.barba@epn.edu.ec>
2026-07-04 18:13:10 -0500

    feat: initialize express server with base routing and middleware configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,6 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import routerUser from './routers/user_routes.js'
 import routerAulas from './routers/aula_routes.js'
-import swaggerUi from 'swagger-ui-express'
-import swaggerSpec from './swagger.js'
 // Inicializaciones
 const app = express()
 dotenv.config()
@@ -19,24 +17,20 @@ app.use(cors())
 
 
 // Variables globales
-app.set('port',process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000)
 
 // Rutas 
-app.get('/',(req,res)=> res.send("Server on"))
+app.get('/', (req, res) => res.send("Server on"))
 
 // Rutas para usuarios
-app.use('/api',routerUser)
-
-<<<<<<< HEAD
-// Esto hará que la documentación sea accesible en la raíz del backend /api-docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/api', routerUser)
 
 // Rutas para aulas virtuales
-app.use('/api',routerAulas)
+app.use('/api', routerAulas)
 
 // Manejo de una ruta que no sea encontrada
-app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404"))
 
 
 // Exportar la instancia de express por medio de app
-export default  app
+export default app
